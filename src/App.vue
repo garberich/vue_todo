381e61b154fbde2dac1727ed69a5c0fe3c96f74e
Richard Ferney Garcia Becerra <richard.garcia@globant.com>
2022-06-23 23:42:22 -0500

    Create the simple app using local data

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,106 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div id="header">
+      <SearchTodo v-on:query-change="querySearch" />
+    </div>
+
+    <div id="main-container">
+      <h2>Todos</h2>
+      <TodoAdd v-on:add-todo="addTodo" />
+      <Todos v-bind:todoslist="copyTodos" v-on:delete-todo="deleteTodo"/>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SearchTodo from './components/SearchTodo.vue'
+import Todos from './components/Todos.vue'
+import TodoAdd from './components/TodoAdd.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Todos,
+    TodoAdd,
+    SearchTodo
+  },
+  methods:{
+    deleteTodo(id){
+      this.todos = this.todos.filter(todo => todo.id != id)
+      this.copyTodos = [...this.todos]
+    },
+    addTodo(todo){
+      this.todos.push(todo)
+      this.copyTodos = [...this.todos]
+    },
+    querySearch(query){
+      if(query.trim() === ''){
+        this.copyTodos = [...this.todos]
+      } else {
+        const temp = this.todos.filter(todo => {
+          return todo.title.toUpperCase().includes(query.toUpperCase())
+        });
+        this.copyTodos = [...temp]
+      }
+    }
+  },
+  data(){
+    return{
+      todos: [
+        {
+          id: 0,
+          title: 'Comprar la cena',
+          completed: false
+        },
+        {
+          id: 1,
+          title: 'Sacar al perro',
+          completed: true
+        },
+        {
+          id: 2,
+          title: 'Estudiar',
+          completed: false
+        },
+        {
+          id: 3,
+          title: 'Terminar Test',
+          completed: true
+        }
+      ],
+      copyTodos: []
+    }
+  },
+  created(){
+    this.copyTodos = [...this.todos]
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  *{
+    box-sizing: border-box;
+  }
+
+  body{
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 1.5em;
+    padding: 0;
+    margin: 0;
+  }
+
+  #main-container{
+    border: solid 1px #ccc;
+    width: 600px;
+    margin: 100px auto;
+  }
+
+  #header{
+    background: black;
+    padding: 10px;
+  }
+
+  h2{
+    padding: 0 10px;
+  }
 </style>
